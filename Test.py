da2baee9316b265a20fefd7164bb958196de0e0c
Diego Garcia <diegogm@hotmail.com>
2017-09-06 00:23:00 -0400

    Added Sep6

---
+++ Test.py
@@ -0,0 +1,5 @@
+price = float(input ("Enter the price of a meal: "))
+tip = price*0.16
+total = price+tip
+print("A 16% tip would be",round(tip,2))
+print("The total including tip would be ",round(total,2))
